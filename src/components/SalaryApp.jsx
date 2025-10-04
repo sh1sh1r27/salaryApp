1b6c3ccec81d43b6cc2490052d573dc30c30aa4b
Ahmed shishir <ahmedshishir1200@gmail.com>
2025-10-04 03:30:08 -0500

    first commit

--- src/components/SalaryApp.jsx
+++ src/components/SalaryApp.jsx
@@ -1,34 +1,66 @@
+import React, { useState } from 'react'
 
 const SalaryApp = () => {
+
+   const [allData, setAllData] = useState([]);
+
+   const [formData, setFormData] = useState({
+    name:"",
+    salary:"",
+    profession:""
+   });
+
+     const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prev => ({
+      ...prev,
+      [name]: value
+    }));
+  };
+
+   const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!formData.name || !formData.salary || !formData.profession) return;
+
+    setAllData(prev => [...prev, formData]); // add new employee
+    setFormData({ name: '', salary: '', profession: '' }); // reset form
+  };
+
   return (
       <div className=' flex flex-col items-center gap-5 '>
           <h1  className='title'>Salary App</h1>
-          <form className='emmployee-form' > 
+          <form className='emmployee-form'  onSubmit={handleSubmit} > 
           
           <input
             className='input'
             type="text"
+            name='name'
             placeholder='Name'
-            name='name'
             required
+            value={formData.name}
+            onChange={handleChange}
           />
 
           <input
-            className='input'
-            type="text"
-            placeholder='Salary'
-            name='name'
-            required
-          />
-          
-
-          <input 
           className='input'
-          type="text" 
-          placeholder='Profession'
+          type="number"
           name='salary'
+          placeholder='Salary'
           required
-          />
+          value={formData.salary}
+          onChange={handleChange}
+        />
+          
+        <input
+          className='input'
+          type="text"
+          name='profession'
+          placeholder='Profession'
+          required
+          value={formData.profession}
+          onChange={handleChange}
+        />
+
           <button className='btn' type="submit">Submit</button>
 
           <div className='flex flex-col items-center gap-5'>
@@ -39,6 +71,18 @@ const SalaryApp = () => {
              <button className='px-3 border cursor-pointer' type="button">Search</button>
           </div>
         </form>
+        <div className="flex flex-col items-center justify-center min-h-screen gap-5">
+         <h1 className='title'>Employee list</h1>
+           <ul>
+            {allData.map((el, idx) => (
+             <li key={idx} className="employee flex gap-5 border p-2 rounded">
+              <span>{el.name}</span>
+              <span>{el.salary}</span>
+              <span>{el.profession}</span>
+             </li>
+            ))}
+         </ul>
+      </div>
     </div>
   )
 }
